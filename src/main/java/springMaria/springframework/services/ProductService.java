da91f09f86271289307fcae0fbe79696436f5106
eithiri <eithiritun1997@gmail.com>
2020-12-18 17:02:10 +0630

    Initial commit

---
+++ src/main/java/springMaria/springframework/services/ProductService.java
@@ -0,0 +1,22 @@
+package springMaria.springframework.services;
+
+import springMaria.springframework.commands.ProductForm;
+import springMaria.springframework.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+public interface ProductService {
+
+    List<Product> listAll();
+
+    Product getById(Long id);
+
+    Product saveOrUpdate(Product product);
+
+    void delete(Long id);
+
+    Product saveOrUpdateProductForm(ProductForm productForm);
+}
